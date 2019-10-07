345278eb89255094f85ddfc7fefdc48fd7606cd7
Michael Stiles <mstiles01@icloud.com>
2019-10-07 09:56:16 -0500

    not done

--- Program.cs
+++ Program.cs
@@ -21,23 +21,55 @@ wordsAndDefinitions.Add("Pell-Well", "in mingled confusion or disorder.");
 
 
 /*
-    Use square brackets to get the definition of two of the
-    words and then output them to the console
+    You want to track the following about each word:
+        word, definition, part of speech, example sentence
+
+    Example of one dictionary in the list:
+    {
+        "word": "excited",
+        "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+        "part of speech": "adjective",
+        "example sentence": "I am excited to learn C#!"
+    }
 */
-Console.WriteLine(wordsAndDefinitions["Frowsy"]);
-Console.WriteLine(wordsAndDefinitions["Pell-Well"]);
+
+// Create dictionary to represent a few words
+Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+// Add each of the 4 bits of data about the word to the Dictionary
+excitedWord.Add("word", "Excited");
+excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+excitedWord.Add("part of speech", "adjective");
+excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+
+// Add Dictionary to your `dictionaryOfWords` list
+
+
+
+// create another Dictionary and add that to the list
+
 
 /*
-    Below, loop over the wordsAndDefinitions dictionary to get the following output:
-        The definition of (WORD) is (DEFINITION)
-        The definition of (WORD) is (DEFINITION)
-        The definition of (WORD) is (DEFINITION)
+    Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+    Example output for one word in the list of dictionaries:
+        word: excited
+        definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+        part of speech: adjective
+        example sentence: I am excited to learn C#!
 */
-foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+
+// Iterate the List of Dictionaries
+foreach ()
 {
-    Console.WriteLine($"The Meaning of {word.Key} is {word.Value}");
+    // Iterate the KeyValuePairs of the Dictionary
+    foreach ()
+    {
+        Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+    }
 }
         }
     }
 }
-// hopefully this pulls
+
