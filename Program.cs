5c4f382278b75d82e82b2a289eab1594360a9524
Michael Stiles <mstiles01@icloud.com>
2019-10-07 11:58:19 -0500

    Doneski

--- Program.cs
+++ Program.cs
@@ -7,47 +7,43 @@ namespace dictionaries
     {
         static void Main(string[] args)
         {
-        /*
-    Create a dictionary with key value pairs to
-    represent words (key) and its definition (value)
-*/
-Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+        // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
 
-// Add several more words and their definitions
-wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#.");
-wordsAndDefinitions.Add("Frowsy", "having a slovenly or uncared-for appearance.");
-wordsAndDefinitions.Add("Abscond", "to depart secretly and hide oneself.");
-wordsAndDefinitions.Add("Pell-Well", "in mingled confusion or disorder.");
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
 
+            // Create dictionary to represent a few words
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
 
-/*
-    You want to track the following about each word:
-        word, definition, part of speech, example sentence
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
 
-    Example of one dictionary in the list:
-    {
-        "word": "excited",
-        "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
-        "part of speech": "adjective",
-        "example sentence": "I am excited to learn C#!"
-    }
-*/
-
-// Create dictionary to represent a few words
-Dictionary<string, string> excitedWord = new Dictionary<string, string>();
-
-// Add each of the 4 bits of data about the word to the Dictionary
-excitedWord.Add("word", "Excited");
-excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
-excitedWord.Add("part of speech", "adjective");
-excitedWord.Add("example sentence", "I am excited to learn C#!");
-
-
-// Add Dictionary to your `dictionaryOfWords` list
-
-
-// hello
+     // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
 // create another Dictionary and add that to the list
+
+Dictionary<string, string> germaneWord = new Dictionary<string, string>();
+germaneWord.Add("word", "germane");
+germaneWord.Add("definition", "being at once relevant and appropriate" );
+germaneWord.Add("part of speech", "adjective");
+germaneWord.Add("example sentence", "Michael's humor used to be very germane, not its antiquated.");
+
+dictionaryOfWords.Add(germaneWord);
+
+
 
 
 /*
@@ -61,13 +57,15 @@ excitedWord.Add("example sentence", "I am excited to learn C#!");
 */
 
 // Iterate the List of Dictionaries
-foreach ()
+foreach (Dictionary<string, string>definitions in dictionaryOfWords)
 {
     // Iterate the KeyValuePairs of the Dictionary
-    foreach ()
+    foreach (KeyValuePair<string, string> wordData in definitions)
     {
         Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+         
     }
+
 }
         }
     }
